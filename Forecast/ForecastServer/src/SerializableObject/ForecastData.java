cdec16a93b5660dc60f0ad4ef5d417c9ed68fde7
Hizkia Adi Surya <hizkia92@gmail.com>
2014-03-09 12:00:08 +0700

    update

---
+++ Forecast/ForecastServer/src/SerializableObject/ForecastData.java
@@ -0,0 +1,62 @@
+package SerializableObject;
+import java.io.Serializable;
+/**
+ *
+ * @author misbahul
+ */
+
+public class ForecastData implements Serializable
+{
+    private static final long serialVersionUID = 1L;
+    private String forecastDay;
+    private String forecastDate;
+    private String forecastWeather;
+    /**
+     * @return the forecastDay
+     */
+    public String getForecastDay() {
+        return forecastDay;
+    }
+
+    /**
+     * @param forecastDay the forecastDay to set
+     */
+    public void setForecastDay(String forecastDay) {
+        this.forecastDay = forecastDay;
+    }
+
+    /**
+     * @return the forecastDate
+     */
+    public String getForecastDate() {
+        return forecastDate;
+    }
+
+    /**
+     * @param forecastDate the forecastDate to set
+     */
+    public void setForecastDate(String forecastDate) {
+        this.forecastDate = forecastDate;
+    }
+
+    /**
+     * @return the forecastWeather
+     */
+    public String getForecastWeather() {
+        return forecastWeather;
+    }
+
+    /**
+     * @param forecastWeather the forecastWeather to set
+     */
+    public void setForecastWeather(String forecastWeather) {
+        this.forecastWeather = forecastWeather;
+    }
+
+    public  ForecastData(String forecastDate,String forecastDay, String forecastWeather)
+    {
+        this.forecastDate=forecastDate;
+        this.forecastDay = forecastDay;
+        this.forecastWeather=forecastWeather;
+    }    
+}
---
+++ Forecast/ForecastServer/src/forecastserver/ClientHandler.java
@@ -0,0 +1,79 @@
+package forecastserver;
+
+/**
+ *
+ * @author Voron
+ */
+
+import java.net.*;
+import SerializableObject.ForecastData;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class ClientHandler
+{
+    private DatagramSocket socket;
+    private DatagramPacket packet;
+    private int port;
+    private ForecastData data;
+    private int clientport;
+    private InetAddress clienthost;
+    private ByteArrayOutputStream baos;
+    private ByteArrayInputStream bais;
+    private ObjectOutputStream oos;
+    private ObjectInputStream ois;
+    private byte[] buffer;
+    private int BUF_SIZE = 1500;    
+    
+    public ClientHandler(int port) throws IOException
+    {
+        this.port = port;
+        baos = new ByteArrayOutputStream();
+        bais = new ByteArrayInputStream(buffer);
+        oos = new ObjectOutputStream(baos);
+        ois = new ObjectInputStream(bais);
+    }
+    
+    public boolean Listen()
+    {
+        try
+        {
+            socket = new DatagramSocket(this.port);
+            packet = new DatagramPacket(buffer, BUF_SIZE);
+            socket.receive(packet);            
+            this.clienthost = socket.getInetAddress();
+            this.clientport = socket.getPort();
+            
+            ois.read(buffer);
+            data = (ForecastData)ois.readObject();
+            
+            return true;
+        }
+        catch(IOException | ClassNotFoundException ex)
+        {
+            System.out.println(ex);
+            return false;
+        }
+    }
+    
+    public boolean Send(ForecastData data)
+    {
+        try
+        {
+            this.data = data;
+            oos.writeObject((Object)data);
+            buffer = baos.toByteArray();
+            packet = new DatagramPacket(buffer, buffer.length, clienthost, clientport);
+            
+            return true;
+        }
+        catch(IOException ex)
+        {
+            System.out.println(ex);
+            return false;
+        }
+    }
+}
