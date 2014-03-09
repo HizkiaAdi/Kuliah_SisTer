ff7f51778af9814bd42dfe3ea8f0b48bb1471c76
misbahulmunir <iskandarthegre4t@gmail.com>
2014-03-09 13:25:04 -0700

    ganti Socket dengan datagram socket

--- Forecast/ForecastClient/src/sisterweatherforecast/SisterWeatherClient.java
+++ Forecast/ForecastClient/src/sisterweatherforecast/SisterWeatherClient.java
@@ -16,6 +16,7 @@ import static java.util.Collections.list;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import SerializableObject.ForecastData;
+import static java.util.Collections.list;
 
 
 /**
@@ -26,7 +27,16 @@ public class SisterWeatherClient {
     private int port;
     private String ipAdrress;
     private Scanner scan;
+    private DatagramSocket dgSocket;
+    private DatagramPacket packet;
     private List<ForecastData> forecastData;
+    private byte[] buffer;
+    private InetAddress adrres;
+    private int buffersize=1500;
+    private ByteArrayOutputStream baos;
+    private ByteArrayInputStream bais;
+    private  ObjectInputStream ois;
+    private ObjectOutputStream oos;
     public SisterWeatherClient(int port,String ipAdrress)
     {
         this.port=port;
@@ -35,18 +45,23 @@ public class SisterWeatherClient {
     public void StartClient() throws ClassNotFoundException, IOException
     { String input;
       Socket clientSocket;
+      adrres= InetAddress.getByName(ipAdrress);
       List<ForecastData> output;
        String outputPrint="";
         try {
-            clientSocket= new Socket(ipAdrress, port);
-            ObjectInputStream ois= new ObjectInputStream(clientSocket.getInputStream());
-            ObjectOutputStream oos= new ObjectOutputStream(clientSocket.getOutputStream());
+            //clientSocket= new Socket(ipAdrress, port);
+            dgSocket= new DatagramSocket();
+            packet= new DatagramPacket(buffer,buffersize,adrres, port);
+            baos = new ByteArrayOutputStream();
+            bais = new ByteArrayInputStream(buffer);
+            ObjectInputStream ois= new ObjectInputStream(bais);
+            ObjectOutputStream oos= new ObjectOutputStream(baos);
             while(true)
             {
               scan=new Scanner(System.in);
               input=scan.nextLine();
-              oos.writeObject(input);
-                    while((output=(List<ForecastData>)ois.readObject())!=null)
+              SendData(input);
+                    while((output=ReceiveData())!=null)
                     {
                         
                         for(ForecastData data:output)
@@ -64,7 +79,45 @@ public class SisterWeatherClient {
          catch (IOException ex) {
             Logger.getLogger(SisterWeatherClient.class.getName()).log(Level.SEVERE, null, ex);
         }
+      
 
+    } 
+    public List<ForecastData> ReceiveData()
+    {  List<ForecastData> data;
+        try
+        {
+            dgSocket = new DatagramSocket(this.port);
+            packet = new DatagramPacket(buffer, buffersize);
+            dgSocket.receive(packet);           
+            ois.read(buffer);
+            data = (List<ForecastData>)ois.readObject();
+            
+            return data;
+        }
+        catch(IOException | ClassNotFoundException ex)
+        {
+            System.out.println(ex);
+            return null;
+        }
+
+        
     }
     
+    public void SendData(String data)
+    {
+      try
+        {
+            oos.writeObject((Object)data);
+            buffer = baos.toByteArray();
+            packet = new DatagramPacket(buffer, buffer.length, adrres, port);
+            dgSocket.send(packet);
+        }
+        catch(IOException ex)
+        {
+            System.out.println(ex);
+            
+        }
+
+    }
+       
 }
